792be5900f9744f3c8d12a976e8bc01ed873ec44
hhamad1 <74683707+hhamad1@users.noreply.github.com>
2022-11-11 20:14:45 -0500

    bug fixes

--- order_book.py
+++ order_book.py
@@ -16,39 +16,39 @@ def process_order(order):
     session.add(order_obj)
     session.commit()
 
-    orders = session.query(Order).all() #Get all orders
-    for order in orders:
-        if order.filled == None and order.buy_currency == order_obj.sell_currency and order.sell_currency == order_obj.buy_currency and (order.sell_amount / order.buy_amount) >= (order_obj.buy_amount / order_obj.sell_amount):
+    orders = session.query(Order).filter(Order.filled == None).all() #Get all orders
+    for existing_order in orders:
+        if existing_order.filled == None and existing_order.buy_currency == order_obj.sell_currency and existing_order.sell_currency == order_obj.buy_currency and (existing_order.sell_amount / existing_order.buy_amount) >= (order_obj.buy_amount / order_obj.sell_amount):
             time_stamp = datetime.utcnow
-            order.filled = time_stamp
+            existing_order.filled = time_stamp
             order_obj.filled = time_stamp
-            order_obj.counterparty_id = order.id
-            order.counterparty_id = order_obj.id
+            order_obj.counterparty_id = existing_order.id
+            existing_order.counterparty_id = order_obj.id
             session.commit()
 
             # create new order
             new_order_obj = {}
             fields.append('creator_id')
-            if order.sell_amount < order_obj.buy_amount:
+            if existing_order.buy_amount < order_obj.sell_amount:
                 new_order_obj['creator_id'] = order_obj.id
                 new_order_obj['sender_pk'] = order_obj.sender_pk
                 new_order_obj['receiver_pk'] = order_obj.receiver_pk
                 new_order_obj['buy_currency'] = order_obj.buy_currency
                 new_order_obj['sell_currency'] = order_obj.sell_currency
-                new_order_obj['buy_amount'] = (order_obj.buy_amount / order_obj.sell_amount) * (order_obj.sell_amount - order.buy_amount) 
-                new_order_obj['sell_amount'] = (order_obj.sell_amount - order.buy_amount)
-                child_order = Order(**{f:order[f] for f in fields})
+                new_order_obj['buy_amount'] = (order_obj.buy_amount / order_obj.sell_amount) * (order_obj.sell_amount - existing_order.buy_amount) 
+                new_order_obj['sell_amount'] = (order_obj.sell_amount - existing_order.buy_amount)
+                child_order = Order(**{f:new_order_obj[f] for f in fields})
                 session.add(child_order)
                 session.commit()
-            elif order.buy_amount > order_obj.sell_amount:
-                new_order_obj['creator_id'] = order.id
-                new_order_obj['sender_pk'] = order.sender_pk
-                new_order_obj['receiver_pk'] = order.receiver_pk
-                new_order_obj['buy_currency'] = order.buy_currency
-                new_order_obj['sell_currency'] = order.sell_currency
-                new_order_obj['buy_amount'] = (order.buy_amount - order_obj.sell_amount)
-                new_order_obj['sell_amount'] = (order.sell_amount / order.buy_amount) * (order.buy_amount - order.sell_amount)
-                child_order = Order(**{f:order[f] for f in fields})
+            elif existing_order.buy_amount > order_obj.sell_amount:
+                new_order_obj['creator_id'] = existing_order.id
+                new_order_obj['sender_pk'] = existing_order.sender_pk
+                new_order_obj['receiver_pk'] = existing_order.receiver_pk
+                new_order_obj['buy_currency'] = existing_order.buy_currency
+                new_order_obj['sell_currency'] = existing_order.sell_currency
+                new_order_obj['buy_amount'] = (existing_order.buy_amount - order_obj.sell_amount)
+                new_order_obj['sell_amount'] = (existing_order.sell_amount / existing_order.buy_amount) * (existing_order.buy_amount - existing_order.sell_amount)
+                child_order = Order(**{f:new_order_obj[f] for f in fields})
                 session.add(child_order)
                 session.commit()
 
